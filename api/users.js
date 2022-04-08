3f3fda54ebc02c5e039399343819771cdeb17518
Ryan Mulready <ryan.mulready@gmail.com>
2022-04-08 14:40:44 -0400

    feat(docs): updated README.md for workshop and removed api method definitions

--- api/users.js
+++ api/users.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /*
     Example user profile API service
     Extended from base nuxt/axios: https://go.nuxtjs.dev/axios
+    methods available are $post, $patch, $delete, $get
     State management found in: ./store/users.js
 */
 export default async (ctx) => {
@@ -9,50 +11,21 @@ export default async (ctx) => {
         baseURL: ctx.$config.AWS_USER_PROFILE_API,
     });
     return {
+        // Create a new user
         create: async ({
             username, email, city, state,
-        }) => {
-            if (!username || !email) {
-                // throwing an error stops the function and bubbles to the caller
-                throw Error('Username and email are required!');
-            }
-            // no await is needed, return is asnyc
-            return api.$post('/users', {
-                username, email, city, state,
-            });
-        },
+        }) => { },
+        // Update an existing user
         update: async (userId, {
             username, email, city, state,
-        }) => {
-            if (!userId) {
-                throw Error('userId is required!');
-            }
-            return api.$patch(`/users/${userId}`, {
-                username, email, city, state,
-            });
-        },
-        delete: async (userId) => {
-            if (!userId) {
-                throw Error('userId is required!');
-            }
-            return api.$delete(`/users/${userId}`);
-        },
-        get: async (userId) => {
-            if (!userId) {
-                throw Error('userId is required!');
-            }
-            return api.$get(`/users/${userId}`);
-        },
-        list: async () => api.get('/users'),
-        // TODO: `search` and `list` could be combined into one
-        // TODO: Add other search params
-        search: async ({ city }) => {
-            if (!city) {
-                throw Error('City is required!');
-            }
-            const params = new URLSearchParams({ city }).toString();
-
-            return api.get(`/users?${params}`);
-        },
+        }) => { },
+        // Delete an existing user
+        delete: async (userId) => { },
+        // Get individual user details
+        get: async (userId) => { },
+        // Get all users
+        list: async () => [],
+        // Search all users based on city
+        search: async ({ city }) => [],
     };
 };
